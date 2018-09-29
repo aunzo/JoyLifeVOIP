d505ed1b30dfeff7c340d49cf1075496e283e809
Patarapon Tohkam <patarapon.t@gmail.com>
2018-09-29 18:51:22 +0700

    initial xcode project

---
+++ JoyLifeVOIP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JoyLifeVOIP
+//
+//  Created by Patarapon  Tokham on 29/9/2561 BE.
+//  Copyright © 2561 Patarapon  Tokham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
